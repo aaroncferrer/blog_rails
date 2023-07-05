57c122a67c1fb3f08b251757514fc9a361898c21
Anthony <arcferrer5@gmail.com>
2023-07-05 19:56:53 +0800

    Update validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :body, presence: true
-    validates :preview, presence: true, uniqueness: true, length: { minimum: 10, maximum: 50 }  
+    validates :preview, presence: true, uniqueness: true, length: { minimum: 10 }  
     validates :img_name, presence: true, uniqueness: true, length: { maximum: 15 }  
 end
